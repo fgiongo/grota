3249adf0ac3b456abe488d4003abf4891bacaa56
fgiongo <fernandogiongooliveira@gmail.com>
2021-07-22 16:57:55 -0300

    Teste de json_decode

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
 include "input-de-eventos.php";
+
+$tipos_de_evento_str = file_get_contents("tipos-de-evento.json");
+$tipos_de_evento_arr = json_decode($tipos_de_evento_str, TRUE);
+print_r($tipos_de_evento_arr);
+echo $tipos_de_evento_arr["receitas"]["tipo_1"]["nome"];
+
+
