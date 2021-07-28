bee7dc22a57cf83997c6e6774431aaecf5eb00b4
fgiongo <fernandogiongooliveira@gmail.com>
2021-07-28 20:34:33 -0300

    Commit

--- json-writer-reader.php
+++ json-writer-reader.php
@@ -1,21 +1,7 @@
 <?php
 
-// aqui entram as funções set_tipos_de_evento e set_eventos
-// elas precisam ser executadas antes das funções get
-// elas vão usar os dados da variável global $_POST para re-escrever os JSON da base de dados
-// isso vai acontecer todas as vezes que a página for 
-
-print_r($_POST);
-
-
-// função para ler e decodificar os dados no JSON dos tipos de evento.
-// retorna os valores da ponta, ou o array inteiro, que contém os atributos e conteúdo HTML
-// necessários para gerar os campos de formulário para cada tipo de evento.
-
-// a estrutura de tipos-de-evento-receitas.json e tipos-de-evento-despesas é de 3 níveis: 
-// ["tipo de evento"]["campo de formulário"]["element" ou "value" ou "type" ou "name_id" ou "label_inner_html"]
-// É boa prática usar a mesma string para o atributo 'nome' e o atributo 'id' em HTML? Vi em algum lugar, achei prático, tenho usado.
-
+// função que retorna os dados de tipos-de-eventos-receitas na forma de array estruturado:
+//["tipo de evento"]["campo de formulário"]["element" ou "value" ou "type" ou "name_id" ou "label_inner_html"]
 // essa função aceita 3 argumentos (os nodes da estrutura) ou nenehum (nesse caso, retorna a estrutura inteira em array)
 function get_tipos_de_evento_receitas($key1 = null, $key2 = null, $key3 = null) {
     
@@ -32,15 +18,39 @@ function get_tipos_de_evento_receitas($key1 = null, $key2 = null, $key3 = null) 
     }
 }
 
-//echo get_tipos_de_evento_receitas("doacao_de_pessoa_fisica", "cpf", "element");
-//echo "\n";
-//print_r (get_tipos_de_evento_receitas());
+// função que retorna um objeto-teste (array) na estrutura de tipos-de-evento-receitas.json
+function gerar_objeto_teste() {
+    $novo_tipo_de_evento = array(
+        "select_option" => array(
+            "value" => "novo-tipo-de-evento",
+            "option_inenr_html" => "Novo Tipo de Evento"
+        ),
+        "valor" => array(
+            "element" => "input",
+            "type" => "text",
+            "name_id" => "novo-tipo-de-evento-valor",
+            "label_inner_html" => "Valor: "
+        ),
+        "data" => array(
+            "element" => "input",
+            "type" => "text",
+            "name_id" => "novo-tipo-de-evento-data",
+            "label_inner_html" => "Data: "
+        )
+        );
+    return $novo_tipo_de_evento;
+}
 
-/*
-// testando abrir um diretório novo e escrever um JSON
-mkdir ("test-directory");
-$testfile = fopen("test-directory/testfile.json", "w");
-$testfile_content = "{\n \"object_key\" : \"object value\"\n }";
-fwrite ($testfile, $testfile_content);
-fclose($testfile);
-*/
+
+// re-escreve tipos-de-evento-receitas.json com o objeto-teste anexado
+function write_to_json($arr) {
+    $tipos_de_evento = get_tipos_de_evento_receitas();
+    array_push($tipos_de_evento, $arr);
+    $tipos_de_evento_receitas_json = fopen("base-de-dados/tipos-de-evento/tipos-de-evento-receitas.json", "w");
+    $tipos_de_evento_str = json_encode($tipos_de_evento, JSON_PRETTY_PRINT);
+    fwrite($tipos_de_evento_receitas_json, $tipos_de_evento_str);
+    fclose($tipos_de_evento_receitas_json);
+}
+
+write_to_json(gerar_objeto_teste());
+
