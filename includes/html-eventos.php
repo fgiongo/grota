21d96ddb9ee7a2509d15cddb30297017940d0a48
fgiongo <fernandogiongooliveira@gmail.com>
2021-08-25 18:20:31 -0300

    re-começando/simplificando

---
+++ includes/html-eventos.php
@@ -0,0 +1,23 @@
+<div id="div-tabela-de-eventos">
+    <table id="tabela-de-eventos">
+        <tr>
+            <th>ID</th>
+            <th>Evento</th>
+            <th>Data</th>
+            <th>Valor</th>
+        </tr>
+    </table>
+</div>
+<div>
+    <h2>Inserir Evento</h2>
+    <form>
+        <div><label for="titulo-novo-evento">Título</label><div>
+        <input type="text" name="titulo-novo-evento" id="titulo-novo-evento">
+
+        <div><label for="data-novo-evento">Data</label><div>
+        <input type="text" name="data-novo-evento" id="data-novo-evento">
+
+        <div><label for="valor-novo-evento">Valor</label><div>
+        <input type="text" name="valor-novo-evento" id="valor-novo-evento">
+    </form>
+</div>
